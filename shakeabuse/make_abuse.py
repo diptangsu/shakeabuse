0c2b27c3313f31bdef6b714ab746f1abd6330ee1
diptangsu <diptangsu.97@gmail.com>
2020-05-26 01:53:34 +0530

    added more abuses

--- shakeabuse/make_abuse.py
+++ shakeabuse/make_abuse.py
@@ -6,6 +6,9 @@ def generate_random_abuse():
     one = random.choice(ONE)
     two = random.choice(TWO)
     three = random.choice(THREE)
+    abuse_parts = f'Thou {one} {two} {three}!'
+    
+    abuse_full = random.choice(FULL_INSULTS)
+    
+    return random.choice((abuse_parts, abuse_full))
 
-    abuse = f'Thou {one} {two} {three}!'
-    return abuse
