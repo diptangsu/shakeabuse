6639fc3e4a415b5b05054c86d5ab609f93deb56b
diptangsu <diptangsu.97@gmail.com>
2020-10-02 16:00:50 +0530

    renamed function from main to abuse and refactored it in console scripts

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "shkabuse=shakeabuse.__main__:main",
+            "shkabuse=shakeabuse.__main__:abuse",
         ]
     },
 )
--- shakeabuse/__main__.py
+++ shakeabuse/__main__.py
@@ -1,13 +1,11 @@
-from make_abuse import generate_random_abuse
+from .make_abuse import generate_random_abuse
 
 
-def main():
-    # type: () -> str
-
+def abuse() -> str:
     random_abuse = generate_random_abuse()
     return random_abuse
 
 
 if __name__ == '__main__':
-    print(main())
+    print(abuse())
 
