dacb07b4bb8468da82a4959e8496fc5587526b3a
Diptangsu Goswami <deep.dippu@gmail.com>
2022-07-19 17:42:53 +0530

    Update __main__.py
    print abuse, return 0

--- shakeabuse/__main__.py
+++ shakeabuse/__main__.py
@@ -3,7 +3,8 @@ from .make_abuse import generate_random_abuse
 
 def abuse() -> str:
     random_abuse = generate_random_abuse()
-    return random_abuse
+    print(random_abuse)
+    return 0
 
 
 if __name__ == '__main__':
